0987389303ec9920d8b8ce5a4df5d1320c5a6ef3
seunshix <adexseun13@Gmail.com>
2022-12-16 13:21:05 -0800

    changed file name. Import error because file name same as library

---
+++ matplotlib/matplotlib_tut.py
@@ -0,0 +1,6 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig, ax = plt.subplots() # Create a figure containing a single axes
+ax.plot([6, 5.4, 2.6, 2.4, 1.8], [5, 4.4, 1.6, 1.4, 0.8])
