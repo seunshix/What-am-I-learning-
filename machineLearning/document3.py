07ddd326699a6dfcd7ca6720dbb06cfc8d12ec51
seunshix <adexseun13@Gmail.com>
2023-01-14 14:30:26 -0800

    beginner nlp

---
+++ machineLearning/document3.py
@@ -0,0 +1,56 @@
+training_doc3 = """ 
+My pussy tastes like Pepsi cola
+My eyes are wide like cherry pies
+I got sweet taste for men who are older
+It's always been so, it's no surprise
+Ah, he's in the sky with diamonds
+And he's making me crazy (I come alive, alive)
+All he wants to do is party with his pretty baby
+Come on, baby, let's ride
+We can escape to the great sunshine
+I know your wife and she wouldn't mind
+We made it out to the other side
+We made it out to the other side
+We made it out to the other side
+Come on, come on
+Come on, come on, come on, baby
+Oh, oh yeah
+I fall asleep in an American flag
+I wear my diamonds on Skid Row
+I pledge allegiance to my dad
+For teaching me everything he knows
+Ah, he's in the sky with diamonds
+And he's making me crazy (I come alive, alive)
+All he wants to do is party with his pretty baby, yeah
+Come on, baby, let's ride
+We can escape to the great sunshine
+I know your wife and she wouldn't mind
+We made it out to the other side
+We made it out to the other side
+We made it out to the other side
+Drugs, suck it up, like vanilla icies
+Don't treat me rough, treat me really niceys
+Decorate my neck, diamantes ices
+Why, come on, come on
+Ooh-ooh, ooh-ooh
+Ooh-ooh, ooh-ooh
+Ooh-ooh, ooh-ooh, ooh, yeah
+Come on, baby, let's ride
+We can escape to the great sunshine
+I know your wife and she wouldn't mind
+We made it out to the other side
+Come on, baby, let's ride
+We can escape to the great sunshine
+We made it out to the other side
+We made it out to the other side
+Come on, come on, come on, baby
+Come on, come on, come on, baby
+Oh, oh
+My pussy tastes like Pepsi cola
+Ooh, ah
+My pussy tastes like Pepsi cola
+Ooh-ooh, ooh-ooh
+Ooh-ooh
+My pussy tastes like Pepsi cola
+Ooh, oh yeah
+"""
